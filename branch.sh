7e7c0b0a9ee49ef8be3c22a68ca11378aaacfa7d
LJuvo <www.704921698@qq.com>
2019-07-16 14:27:20 +0800

    add branch.sh

---
+++ branch.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+int=1
+while(( $int<=5 ))
+do
+    printf "\n操作选项\n\n"
+    printf "1):查看所有分支\n"
+    printf "2):创建新分支\n"
+    printf "3):删除分支\n"
+    printf "4):提交分支代码\n"
+    printf "5):退出\n\n"
+
+    echo -n "选择操作:"
+    read checkNum
+    case $checkNum in 
+        1)  git branch -a
+        ;;
+        2)  echo -n "创建新分支(分支名):"
+            read newBranchName
+            git branch $newBranchName
+            git checkout $newBranchName
+            git push origin "$newBranchName":"$newBranchName"
+        ;;
+        3)  echo -n "删除分支请输入分支名："
+            read branchName
+            echo -n "切换master分支"
+            git checkout master
+            echo -n "删除本地分支:$branchName"
+            git branch -D $branchName
+            echo -n "删除远端分支:$branchName"
+            git push origin --delete $branchName
+        ;;
+        4)  
+            function gitBranch {
+            br=`git branch | grep "*"`
+            echo ${br/* /}
+            }
+
+            git add -A
+            echo -n "输入commit内容："
+            read commitInfo
+            git commit -m "$commitInfo"
+            git push origin gitBranch
+        ;;
+        5)  exit
+        ;;
+        *)  echo '你没有输入 1 到 4 之间的数字'
+        ;;
+    esac
+done
